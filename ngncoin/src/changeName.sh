ffc1ede6f65faaaeb623fdb267a7bbc7fb11ec4c
ShinSeongCheol <cher3725@naver.com>
2021-10-01 20:01:34 +0900

    Create a new Cryptocurrency using litecoin I made a ngncoin

---
+++ ngncoin/src/changeName.sh
@@ -0,0 +1,8 @@
+find . -type f -print0 | xargs -0 sed -i 's/ngncoin/ngncoin/g'
+find . -type f -print0 | xargs -0 sed -i 's/Ngncoin/Ngncoin/g'
+find . -type f -print0 | xargs -0 sed -i 's/NgnCoin/NgnCoin/g'
+find . -type f -print0 | xargs -0 sed -i 's/NGNCOIN/NGNCOIN/g'
+find . -type f -print0 | xargs -0 sed -i 's/NGN/NGN/g'
+
+find . -type f -print0 | xargs -0 sed -i 's/23333/23333/g'	
+find . -type f -print0 | xargs -0 sed -i 's/23322/23322/g'
---
+++ ngncoin/src/key.sh
@@ -0,0 +1,6 @@
+openssl ecparam -genkey -name secp256k1 -out alertkey.pem
+openssl ec -in alertkey.pem -text > alertkey.hex
+openssl ecparam -genkey -name secp256k1 -out testnetalert.pem
+openssl ec -in testnetalert.pem -text > testnetalert.hex
+openssl ecparam -genkey -name secp256k1 -out genesiscoinbase.pem
+openssl ec -in genesiscoinbase.pem -text > genesiscoinbase.hex
